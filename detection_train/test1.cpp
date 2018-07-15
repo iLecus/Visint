ab7a1aeea832b6a32999275143f2cb94ac26d1b0
cw <296083364@qq.com>
2018-07-15 15:19:33 +0800

    基础文件上传

---
+++ detection_train/test1.cpp
@@ -0,0 +1,27 @@
+﻿#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+void EnumerateCameras()
+{
+	Mat img = imread("C:\\Users\\Administrator\\Pictures\\98486.jpg");
+	
+	// 在窗口中显示游戏原画
+	
+	// 等待6000 ms后窗口自动关闭
+	
+
+	system("pause");
+
+}
+
+int main()
+{
+	
+	EnumerateCameras();
+
+	return 0;
+}
